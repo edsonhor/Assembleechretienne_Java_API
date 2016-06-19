30875595d25401defd967085b2dfbdbe943d246f
Edson H Philippe <edson.philippe@gmail.com>
2016-06-19 00:43:54 -0400

    Simplify the Mail Unit Test

--- src/main/java/UnitTest/mailService.java
+++ src/main/java/UnitTest/mailService.java
@@ -1,7 +1,5 @@
 package UnitTest;
 
-import static org.junit.Assert.*;
-
 import javax.mail.MessagingException;
 
 import org.junit.Test;
@@ -12,9 +10,8 @@ public class mailService {
 
 	@Test
 	public void testSendEmail() {
-		MailService unitUndertest= new MailService();
 		try {
-			unitUndertest.sendEmail("edson.philippe@ufl.edu", "This is a test", "If you got this mail, then you should be happy");
+			MailService.sendEmail("edson.philippe@ufl.edu", "This is a test", "If you got this mail, then you should be happy");
 		} catch (MessagingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
